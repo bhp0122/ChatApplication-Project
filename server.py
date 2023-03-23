71e7a75dd3cc752328090528d84397e65799b87f
bhp0122 <105461976+bhp0122@users.noreply.github.com>
2023-03-22 22:56:07 -0500

    Update server.py

--- server.py
+++ server.py
@@ -17,78 +17,98 @@ server.listen()
 print('Server listening...')
 # Accepts the incoming requests
 
-clients = []
+connected_clients = []
 # Keeps track of all connected clients
 
+usernames = []
+# Keeps track of the user's nicknames 
 
 
 def handle():
     # Handles all the incoming connections and establishes them
     while True:
-        client, address = server.accept()
+        client_connection, address = server.accept()
         # Connects to the client side where it is defined as .connect()
         # Will remain available to listen for more connections
 
-        client.send('Name: '.encode('ascii'))
+        client_connection.send('NAME: '.encode('ascii'))
         # .send() is only used with a connected socket ∴ uses the client socket to send
         # Asks name from the client to turn into nickname.
 
-        name = client.recv(1024).decode('ascii')
+        name = client_connection.recv(1024).decode('ascii')
         # Receives the client's name
 
         nickname = f"{name}{random.randint(0, 9)}"
         # Assigned nickname to client: name + number
 
-        clients.append(nickname)
-        # Adds connected client
-
         print(f"Connected with {name} as {nickname}: {address}")
         # Shows the name and address of newly connected client only to the server
 
-
-        client.send('Connected to server.'.encode('ascii'))
+        client_connection.send('Connected to server as {}...'.format(nickname).encode('ascii'))
         # Sends to newly connected client that they are connected to the server
         # .encode() necessary because sockets only send bytes between each other therefore just a string would not be accepted
 
-        client.send('Nickname is {}.'.format(nickname).encode('ascii'))
-        # Inform client of nickname established
-
         message = '{} has joined'.format(nickname)
-        broadcast(message.encode('ascii'))
+        broadcast(message)
         # Notifies everyone that a newly connected client has joined
 
+        usernames.append(nickname)
+        # Adds connected client
 
-        forwarding_thread = threading.Thread(target = forwarding, args = (client, ))
+        connected_clients.append(client_connection)
+        # adds the client's connection to the list
+
+        print(connected_clients)
+
+        forwarding_thread = threading.Thread(target = forwarding, args = (client_connection, ))
         forwarding_thread.start()
-        
+    
 def broadcast(message):
-    for client in clients:
-        client.send(message)
-        # Sends the message to every connected user
+    for c in connected_clients:
+        c.send(message.encode('ascii'))
+
+def send(message, receiver):
+    client_index = usernames.index(receiver)
+    receiver_connection = connected_clients[client_index]
+    receiver_connection.send(message)
+
 
 def forwarding(client_connection):
     # Receives the message by the user and sends it to other users
-    while True:
+    while True:   
+        client_connection.send('Receiver:'.encode('ascii'))
+        receiver = client_connection.recv(1024)
+        
+        client_connection.send('To {}: {}'.format(receiver, input(' ')).encode('ascii'))
         message = client_connection.recv(1024)
         # Receives message of up to 1024 bytes
 
+        send(message, receiver)
+        # forwards message to desired person
+
         if message == '.exit':
         # if the client decides to disconnect
-            client_index = clients.index(client_connection)
-            client_connection.send('Leaving...'.encode('ascii'))
-
-
-            broadcast('{} has left'.format(clients[client_index]).encode('ascii'))
-            # inform other clients
-
-            print(f"{clients[client_index]} has left.")
-            clients.remove(client_index)
+            client_index = connected_clients.index[client_connection]
+            
+            client_connection.send('You have disconnected.'.encode('ascii'))
             
             # close the disconnecting clients socket
             client_connection.close()
-            break
 
-        broadcast(message)
-        # Sends to all the other clients connected
+            connected_clients.remove(client_connection)
+            # removes the client's connection for list
+            
+            broadcast('{} has left.'.format(usernames[client_index]))
+            # inform other clients that a client has left
+
+            print(f"{usernames[client_index]} has left.")
+            # Let's server know that a client has left. 
+
+            usernames.remove(client_index)
+
+            print(connected_clients)
+            break 
+
+
 
 handle()
