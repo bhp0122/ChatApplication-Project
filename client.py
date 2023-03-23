c412153b50050b40b522144e4143213e70ab38c0
precious832 <123005175+precious832@users.noreply.github.com>
2023-03-23 18:01:28 -0500

    Update client.py

--- client.py
+++ client.py
@@ -1,34 +1,39 @@
-# This is the code for the client side
 import socket
 
 BUFF_SIZE = 1024
 PORT = 9999
 IP = '127.0.0.1'
-# create the client socket
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print("Client socket created")
 
-# connect socket to address (server)
-# host = socket.gethostname() # hello-2.uom.memphis.edu
-# ip = socket.gethostbyname(host)
-
-client_socket.connect((IP, PORT))
-print(f"Client {client_socket} now connected to server") # prints the socket information, may remove
-
-# Bhumi: After the client_socket.connect, I'll need the client to input their name so I can pair the name with the random number
-
-
-
-# create and send message
-message = input("Start chatting: ")
-while message != ".exit":
-    client_socket.sendall(message.encode('utf-8')) # Bhumi: Is sendall used for a TCP connection because I thought I was used for UDP connection. 
-
-    # receive any messages and display them
+# receive any messages and display them
+def receive_display_message():
     message_rcd = client_socket.recv(BUFF_SIZE).decode('utf-8')
-    print(message_rcd)
-    message = input("--> ") # take input for next message
+    if not message_rcd:
+        return 0
+    else:
+        print(message_rcd)
+        return 1
 
-client_socket.close() # close the chat if client enters ".exit"
 
+if __name__ == "__main__":
+    # create the client socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("Client socket created")
+
+    client_socket.connect((IP, PORT))
+    print(f"Client {client_socket} now connected to server") # prints the socket information, may remove
+
+    # create and send message
+    message = ''
+    # get username from user
+
+    print("Enter your username: \n")
+    # continue to take input until user enters ".exit"
+
+    while message != ".exit":
+        message = input("--> ") # first time gets username
+        client_socket.send(message.encode('utf-8'))
+        if receive_display_message() == 0:
+            message = input("--> ") # takes input for next message
+
+    client_socket.close() # close the chat if client enters ".exit"
 
