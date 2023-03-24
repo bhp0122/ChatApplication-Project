f5df0f8ca72bd76ea3d5bea11bcc832bf505552d
bhp0122 <105461976+bhp0122@users.noreply.github.com>
2023-03-23 20:50:34 -0500

    Update client.py

--- client.py
+++ client.py
@@ -21,6 +21,8 @@ if __name__ == "__main__":
 
     client_socket.connect((IP, PORT))
     print(f"Client {client_socket} now connected to server") # prints the socket information, may remove
+    # Bhumi: I don't think the client will need this. 
+   
 
     # create and send message
     message = ''
@@ -28,8 +30,35 @@ if __name__ == "__main__":
 
     print("Enter your username: \n")
     # continue to take input until user enters ".exit"
-
+    
+    '''
+    Bhumi: Since the server needs the user's username, it will have to receive it from the person so it can let everyone else know so anyone can send a message 
+    
+    Ex. if client_socket.recv(1024).decode('ascii') == 'Name':
+            name = input('Please enter your name --> ')
+            client_socket.send(name.encode('ascii'))
+    '''
+    
+    
     while message != ".exit":
+        '''
+        Bhumi: Before the client is able to read input, the server needs to let the user know its connected
+        and what clients are already connected. The server also needs to know who the message is going to be for.
+        
+        Ex. messsage_rcv = client_socket.recv(1024).decode('ascii')
+        
+            if message_received == 'Receiver':
+                receiver = input('Who would you like to send this message to? ')
+                client_socket.send(receiver.encode('ascii'))
+            if message_received == 'Message':
+                message = input(f'To {receiver}: ')
+                client_socket.send(message.encode('ascii'))
+            elif message_received != 'Receiver' and message_received != 'Message':
+                print(message_received)
+        
+        This is what I used. The above worked to some extent, but it was still buggy.   
+        '''
+        
         message = input("--> ") # first time gets username
         client_socket.send(message.encode('utf-8'))
         if receive_display_message() == 0:
