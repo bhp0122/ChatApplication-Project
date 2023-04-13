ad4862e2294b7e6626490396683fe3196eb550d5
bhp0122 <105461976+bhp0122@users.noreply.github.com>
2023-04-12 22:56:15 -0500

    Update client.py

--- client.py
+++ client.py
@@ -1,47 +1,73 @@
 import socket
 import threading
+import sys
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('127.0.0.1', 9999))
+BUFF_SIZE = 1024
+PORT = 9999
+IP = '127.0.0.1'  # localhost
 
-if client_socket.recv(1024).decode('ascii') == 'NAME':
-    name = input('Name --> ')
-    client_socket.send(name.encode('ascii'))
-    print('Enter ".exit" to exit.')
-    print('Enter "change" to switch who you would like to send a message to.')
+running = True
 
-def receive_display_messages():
+# receive any messages and display them
+def receive_display_message(running):
     while True:
-        received_message = client_socket.recv(1024).decode('ascii')
-        if received_message not in ['RCVR', 'RCVR DNE', 'MSG']:
-            print(f"{received_message}")
+        message_rcd = client_socket.recv(BUFF_SIZE).decode('utf-8')
+        if message_rcd:
+            if message_rcd not in ['RCVR', 'RCVR DNE', 'MSG', '.exit', 'DISCONNECTING']:
+                print(f'{message_rcd}\n')
+                client_socket.send('x'.encode('utf-8'))
+            else:
+                handle_message(message_rcd)
+
+
+
+def handle_message(m):
+    if m == 'RCVR':
+        print("Who would you like to send a message to? ")
+        return True
+    elif m == 'MSG':
+        print("Message: ")
+        return True
+    elif m == 'DISCONNECTING':
+        print('You are disconnecting.')
+        sys.exit()
+
+   
+if __name__ == "__main__":
+
+    # create the client socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("Client socket created")
+
+    client_socket.connect((IP, PORT))
+    print(f"Client now connected to server")
+
+    # get username from user
+    if client_socket.recv(1024).decode('ascii') == 'NAME':
+        name = input('Name --> ')
+        client_socket.send(name.encode('utf-8'))
+        print('Enter ".exit" to exit.')
+        print('Enter "change" to switch who you would like to send a message to.')
+    
+    # continue to take input until user enters ".exit"
+    message = ''
+
+    receiving_thread = threading.Thread(target=receive_display_message, args=[running, ])
+    receiving_thread.start()
+
+    take_input = True
+    while True:
+        if take_input:
+            message = input(' \n')
+            take_input = False
         else:
-            output = handle(received_message)
-            if output == 0:
-                return
+            take_input = True
+            client_socket.send(''.encode('utf-8'))
+            continue
+
+        client_socket.send(message.encode('utf-8'))
+
+    
 
 
-def handle(received_message):
-    rcvr = ''
-    if received_message == 'RCVR':
-        rcvr = input('Who would you like to send a message to? ')
-        
-        if rcvr == '.exit':
-            client_socket.send(rcvr.encode('ascii'))
-            return 0
-        else:
-            client_socket.send(rcvr.encode('ascii'))
-
-    if received_message == 'MSG':
-        msg = input('\nMessage: ')
-
-        if msg == '.exit':
-            client_socket.send(rcvr.encode('ascii'))
-            return 0
-        else:
-            client_socket.send(msg.encode('ascii'))
-
-
-
-receiving_thread = threading.Thread(target=receive_display_messages)
-receiving_thread.start()
+    
