4d0c07b19f3b6b8dd8cb9ed27d5bbffa1a7deafc
Precious Jones <precious36926@gmail.com>
2023-04-16 20:45:53 -0500

    updated client

--- client.py
+++ client.py
@@ -7,38 +7,42 @@ PORT = 9999
 IP = '127.0.0.1'  # localhost
 
 running = True
+stop = False
+
 
 # receive any messages and display them
 def receive_display_message(running):
     while True:
         message_rcd = client_socket.recv(BUFF_SIZE).decode('utf-8')
         if message_rcd:
-            if message_rcd not in ['RCVR', 'RCVR DNE', 'MSG', '.exit', 'DISCONNECTING']:
+            if not any(i in message_rcd for i in ['RCVR', 'RCVR DNE', 'MSG', '.exit', 'DISCONNECTING']) :
                 print(f'{message_rcd}\n')
                 client_socket.send('x'.encode('utf-8'))
             else:
                 handle_message(message_rcd)
 
-
-
+# display the appropriate message depending on the message from the server
 def handle_message(m):
-    if m == 'RCVR':
+    if 'RCVR' in m:
         print("Who would you like to send a message to? ")
         return True
-    elif m == 'MSG':
+    elif 'MSG' in m:
         print("Message: ")
         return True
     elif m == 'DISCONNECTING':
         print('You are disconnecting.')
-        sys.exit()
+        global stop
+        stop = True
+        exit()
 
-   
+
 if __name__ == "__main__":
 
     # create the client socket
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print("Client socket created")
 
+    # connect client to server
     client_socket.connect((IP, PORT))
     print(f"Client now connected to server")
 
@@ -48,26 +52,25 @@ if __name__ == "__main__":
         client_socket.send(name.encode('utf-8'))
         print('Enter ".exit" to exit.')
         print('Enter "change" to switch who you would like to send a message to.')
-    
-    # continue to take input until user enters ".exit"
+
     message = ''
 
+    # thread to check for message from server
     receiving_thread = threading.Thread(target=receive_display_message, args=[running, ])
     receiving_thread.start()
 
     take_input = True
-    while True:
+    # to continue to take input from the user until stop is True
+    while not stop:
         if take_input:
             message = input(' \n')
             take_input = False
         else:
             take_input = True
-            client_socket.send(''.encode('utf-8'))
+            client_socket.send(''.encode('utf-8')) # send the message to the server
             continue
 
         client_socket.send(message.encode('utf-8'))
 
-    
 
 
-    
